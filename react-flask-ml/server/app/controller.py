56b17d23123654b8a4d83f9ff987ff5acc984907
kmurphs <kibongesp@gmail.com>
2019-08-14 21:27:51 +0200

    Logisitc Regression integrated with 2 endpoints. Tested with postman

--- react-flask-ml/server/app/controller.py
+++ react-flask-ml/server/app/controller.py
@@ -1,15 +1,63 @@
 # views.py
 
-from flask import render_template
-
 from app import app
 from app.views import views
+from app.ml_model import model
+from flask import request
+import numpy as np
 
+def init():
+	# print(app.config)
+	# print(app.config['DATASET_FILEPATH'])
+	model.train()
+
+
+# https://flask.palletsprojects.com/en/1.1.x/api/#flask.Request
+# request.args is to get urls arguments  GET
+# request.form to get form parameter POST
 @app.route('/')
 def index():
-    return views.index()
+	print(train.hello())
+	return views.index()
+
+
+@app.route('/api/predict', methods = ['POST'])
+def api_predict():
+	
+	# data = request.get_json(force=True)
+	# print(data)
+	# print(data['foo'])
+
+	data_format = request.args['format']
+	if(data_format == 'csv'):
+		print("in csv")
+		# print(request.get_data())
+		# print(request.values)
+		# print(request.form)
+		# print(request.files)
+
+		# print(type(request.get_data()))
+		# print(repr(request.get_data().decode("utf-8")))
+		data = request.get_data().decode("utf-8").split('\r\n')
+		data = [item.split(',') for item in data]
+		data = np.asarray(data, dtype=np.float32)
+		pred = model.predict(data)
+
+		predictions = ["{} is predicted to represent '{}'".format(data[i,:], pred[i]) for i in range(pred.shape[0]) ]
+		predictions = '\n'.join(predictions)
+	else:
+		print("in default")
+
+	return predictions
+
+
+@app.route('/data/predict', methods = ['POST'])
+def form_predict():
+	print(request.form.get('foo'))
+
+	return views.about()
 
 
 @app.route('/about')
 def about():
-    return views.about()
+	return views.about()
--- react-flask-ml/server/config.py
+++ react-flask-ml/server/config.py
@@ -1,4 +1,19 @@
 # config.py
 
 # Enable Flask's debugging features. Should be False in production
-DEBUG = True
+# DEBUG = True
+
+
+class Config(object):
+    DEBUG = True
+    DEVELOPMENT = True
+    SECRET_KEY = 'do-i-really-need-this'
+    FLASK_HTPASSWD_PATH = '/secret/.htpasswd'
+    FLASK_SECRET = SECRET_KEY
+    DB_HOST = 'database' # a docker link
+    DATASET_FILEPATH ='app/ml_model/dataset/iris.data'
+
+class ProductionConfig(Config):
+    DEVELOPMENT = False
+    DEBUG = False
+    DB_HOST = 'my.production.database' # not a docker link
