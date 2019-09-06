f0085437ae7fb15797f759ef33230e625566ddbb
stephane kibonge <stephane.kibonge@etion.co.za>
2019-09-06 09:44:13 +0200

    Initial commit

--- Secure REST API/common/config/env.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    "port": 5000,
-    "host": "localhost",
-    "jwt_secret": "myS33!!creeeT",
-    "jwt_expiration_in_seconds": 36000
-}
--- react-flask-ml/server/app/views/views.py
+++
@@ -1,9 +0,0 @@
-# views.py
-
-from flask import render_template
-
-def index():
-    return render_template("index.html")
-
-def about():
-    return render_template("about.html")
