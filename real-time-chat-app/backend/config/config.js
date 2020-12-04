fa6eb0a24506fb1ce14bfe20a9b3dec2dd73599f
stephane kibonge <stephane.kibonge@etion.co.za>
2020-12-04 08:17:17 +0200

    REset Readme

--- real-time-chat-app/backend/config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	port: 5000,
-	DB_URL: "mongodb://localhost:27017/chat"
-}
--- real-time-chat-app/backend/models/connect.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.Promise = require("bluebird");
-
-module.exports = mongoose.connect(require("../config/config").DB_URL, { 
-	useNewUrlParser: true 
-});
